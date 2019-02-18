1d59a7ab8fa85102f33f8ad0422717b0322265aa
malexn <alexander.nordh@gmail.com>
2019-02-18 14:02:58 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+var dataArray = [];
+var circles = [];
+var regioncnt = {};
+
+//list_id ;  platform  ; first_date ; last_date  ; region ; category;sub_category; subject_full ;  area  ; zipcode ;
+d3.tsv("./data/data.tsv").then(function(data){
+  data.forEach( d => regionCount(d));
+  //console.log(regioncnt);
+
+  var maxvalue = 0;
+  for (var key in regioncnt){
+    //console.log("tjoao " + regioncnt[key] + " " + key  );
+      if (parseInt(regioncnt[key])>maxvalue){
+        maxvalue =parseInt(regioncnt[key]);
+      }
+    }
+  //console.log(maxvalue,"ble");
+  d3.selectAll("g").datum((d,i,k) => { return k[i];}).attr("fill", function (d){
+    //console.log(d.id.replace("a", ""));
+        return d3.color("lightblue").darker(-1*(1-(regioncnt[d.id.replace("a", "")]*(20/(maxvalue)))));
+        //return "green";
+
+  });
+
+});
+
+function regionCount(data) {
+  //console.log(data.region);
+  if (data.region in regioncnt) {
+    //console.log("hej " + data.region);
+    regioncnt[data.region] = regioncnt[data.region] + 1;
+  }
+  else {
+    regioncnt[data.region] = 1;
+  }
+//  colorMap(regioncnt);
+}
+
+function colorMap (regioncnt) {
+  d3.selectAll("path").data(regioncnt),attr("fill", d =>
+    console.log(regioncnt + "hej")
+  )
+}
+
+
+$('.ui.dropdown').dropdown({
+  allowAdditions: true
+});
+
+function filterCategories(data) {
+  var selector = document.getElementById("cat");
+  var value = selector[selector.selectedIndex].value;
+  console.log(value);
+
+}
